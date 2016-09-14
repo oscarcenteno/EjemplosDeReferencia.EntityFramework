26a5858b38ecb8ee28389f6fc78a7f15b1bb65c9
Oscar <oscar.centeno@outlook.com>
2016-09-14 11:52:54 -0600

    Ajusto escenarios

--- Ejemplos.EntityFramework.WebApplication/Controllers/CuentasController.cs
+++ Ejemplos.EntityFramework.WebApplication/Controllers/CuentasController.cs
@@ -12,10 +12,10 @@ namespace Ejemplos.EntityFramework.WebApplication.Controllers
             Escenario.AgregueUnaCuenta("Cuenta3", 3, 2);
             Escenario.AgregueUnaCuenta("Cuenta4", 4, 19);
             Escenario.EditeLaCuenta(1, 1, "Cuenta1 Nueva");
-            Escenario.EditeLaCuenta(1, 1, "Cuenta1 Nueva2");
-            Escenario.EditeLaCuenta(2, 1, "Cuenta4 Nueva");
-            Escenario.EditeLaCuenta(3, 2, "Cuenta4 Nueva");
+            Escenario.EditeLaCuenta(1, 1, "Cuenta1 Nueva Nueva");
+            Escenario.EditeLaCuenta(2, 1, "Cuenta2 Nueva");
             Escenario.EditeLaCuenta(4, 19, "Cuenta4 Nueva");
+            Escenario.EditeLaCuenta(4, 19, "Cuenta4 Nueva Nueva");
 
             return View();
         }
